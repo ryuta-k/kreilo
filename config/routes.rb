3ae53a56c4968c794a3f13b7f2b9c5e9edfee863
Jordi <mumismo@gmail.com>
2009-05-08 01:42:24 +0900

    Home controller for the main page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => "users"
   map.resources :users
   map.resource :user_session
-  map.root :controller => "user_sessions", :action => "new"
+  map.root :controller => "home", :action => "index"
 #  map.root :controller => "home", :action => "index"
 end
 
