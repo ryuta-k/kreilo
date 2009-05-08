a1b5a7f327a59fce240fc91b59eb3698cdbd7415
Jordi <mumismo@gmail.com>
2009-05-08 09:49:50 +0900

    Site object globally accesible

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,8 @@
-# Where can I put this so is general available?
-Dir["#{RAILS_ROOT}/lib/kreilo/*.rb"].each do |file|
-  require_dependency file
-end
-
 #TODO: site should be created only once
 class HomeController < ApplicationController
 def index
-  @site ||= Kreilo::Site.new
-  
+#  @site ||= Kreilo::Site.new
+   
   #game_id=@@framework.start_game_type("game1")
 #  while framework.games[game_id].alive?
 #    puts framework.games[game_id].running_time 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+# This should be generally availablefor everyone
+Dir["#{RAILS_ROOT}/lib/kreilo/*.rb"].each do |file|
+  require_dependency file
+end
+
+#site is created only once, can be accessed globally
+$site ||= Kreilo::Site.new
+
+
 end
