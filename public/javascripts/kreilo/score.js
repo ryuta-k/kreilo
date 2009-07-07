4363ca6f3a9d8da177b4b1f2ed35abed8639cffa
tkosuge <takayuki.kosuge@gmail.com>
2009-07-08 07:38:31 +0900

    change score.js

--- public/javascripts/kreilo/score.js
+++ public/javascripts/kreilo/score.js
@@ -1,12 +1,15 @@
 function ease(score, scoreBar, max){
-    $(scoreBar).innerHTML = "";
+    //    $(scoreBar).innerHTML = "";
     var div = $(score);
     var point = div.innerHTML;
     var pBar = new ProgressBar($(scoreBar), {classProgressBar: 'progressBar', style: ProgressBar.DETERMINATE, widthIndicators:3,noIndeterminteIndicators:0,selection:1, color:{r:50, g:200, b:200}});
+
+    // score animation
     setTimeout(function(){
-            var transObj = new Effect.Transform([{".indicator": 'width:' + (300*(point/max)) +'px'}],{duration:3});
+            var transObj = new Effect.Transform([{".indicator": 'width:' + (100*(point/max)) +'%'}],{duration:3});
         transObj.play();
             }, 50);
-    
+
+    // point FadeIn
     new Effect.Fade("score", {from:0.0, to:0.8, delay:2, duration:1});
 }
